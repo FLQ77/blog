b799d998071e4b1ec1729bfe2991bf33d5912a2a
FLQ77 <1249003920@qq.com>
2023-02-10 17:21:43 +0800

    添加theme配置文件

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,4 +5,18 @@ export default defineConfig({
     description: 'a personal blog',
     base: '/blog/',
 
+    themeConfig: {
+        logo: '/logo.svg',
+        nav: [
+            { text: '首页', link: '/'},
+            { text: '学习笔记', link: '/note/'},
+            { text: '问题总结', link: '/issues/'},
+            { text: '生活感悟', link: '/life/'}
+        ],
+        socialLinks: [{ icon: "github", link: "https://github.com/FLQ77" }],
+        footer: {
+        message: "Released under the MIT License.",
+        copyright: "Copyright © 2023-02-09～present FLQ",
+        },
+    }
 })
